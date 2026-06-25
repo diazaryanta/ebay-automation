5f5d9b20ee8db893092c0e5b588752bc0635507b
diaz <diazaryanta196@gmail.com>
2026-06-25 16:26:22 +0700

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 group = "com.diazaryanta"
 version = "1.0-SNAPSHOT"
 
-// Mengatur versi Java ke 21
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
@@ -16,13 +15,8 @@ repositories {
 }
 
 dependencies {
-    // TestNG untuk framework pengujian
     testImplementation("org.testng:testng:7.10.2")
-
-    // Selenium untuk UI Automation
     testImplementation("org.seleniumhq.selenium:selenium-java:4.21.0")
-
-    // RestAssured untuk API Automation (jika skenario 2 nanti API)
     testImplementation("io.rest-assured:rest-assured:5.4.0")
     testImplementation("io.rest-assured:json-path:5.4.0")
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
