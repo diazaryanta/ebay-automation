86bd00a72c856a2ecdbd6f965b6b198b169ec29f
diaz <diazaryanta196@gmail.com>
2026-06-30 07:58:26 +0700

    add: ci/cd pipeline configuration

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -17,6 +17,11 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
 
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(ConfigReader.getProperty("ui.ebay.url"));
